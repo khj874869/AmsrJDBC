76c55d44ca9acba3ba42dea8010bd1154fdc694f
khj874869 <134674147+khj874869@users.noreply.github.com>
2023-07-25 17:20:31 +0900

    Update Studentrun.java

--- src/com/kh/jdbc/day04/run/Studentrun.java
+++ src/com/kh/jdbc/day04/run/Studentrun.java
@@ -1,12 +1,12 @@
 package com.kh.jdbc.day04.run;
 
-import com.kh.jdbc.day04.view.Student_View;
+import com.kh.jdbc.day04.view.StudentView;
 
-public class Studentrun {
 
-	public static void main(String[] args) {
-		Student_View sv = new Student_View();
-		sv.studentProgram();
+public class Run {
+	public static void main(String [] args) {
+		StudentView view = new StudentView();
+		view.startProgram();
 	}
-
 }
+
